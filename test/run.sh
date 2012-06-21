f9898b7d679b449e8e2937b5adab555384a7cb33
Gleb Averchuk <altermn@gmail.com>
2012-06-21 14:14:09 +0400

    test run script

---
+++ test/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+test_mc_dir='/home/newmen/c++/Qt/qt_mc/test'
+
+${test_mc_dir}/test_mc 100 100 1 true
+
+${test_mc_dir}/test_mc 20 20 10
+${test_mc_dir}/test_mc 40 40 10
+${test_mc_dir}/test_mc 60 60 10
+${test_mc_dir}/test_mc 80 80 10
+${test_mc_dir}/test_mc 100 100 10
