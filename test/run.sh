ac86b762ab64a9767d3af44beeff72ed68a8da44
Gleb Averchuk <altermn@gmail.com>
2012-06-21 20:17:43 +0400

    now you must specify a directory for the results

--- test/run.sh
+++ test/run.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 test_mc_dir='/home/newmen/c++/Qt/qt_mc/test'
+result_dir=${test_mc_dir}/results
 
-${test_mc_dir}/test_mc 100 100 1 true
+${test_mc_dir}/test_mc ${result_dir} 200 200 1 true
 
-${test_mc_dir}/test_mc 20 20 10
-${test_mc_dir}/test_mc 40 40 10
-${test_mc_dir}/test_mc 60 60 10
-${test_mc_dir}/test_mc 80 80 10
-${test_mc_dir}/test_mc 100 100 10
+${test_mc_dir}/test_mc ${result_dir} 20 20 10
+${test_mc_dir}/test_mc ${result_dir} 40 40 10
+${test_mc_dir}/test_mc ${result_dir} 60 60 10
+${test_mc_dir}/test_mc ${result_dir} 80 80 10
+${test_mc_dir}/test_mc ${result_dir} 100 100 10
+${test_mc_dir}/test_mc ${result_dir} 120 120 10
+${test_mc_dir}/test_mc ${result_dir} 140 140 10
+${test_mc_dir}/test_mc ${result_dir} 160 160 10
+${test_mc_dir}/test_mc ${result_dir} 180 180 10
+${test_mc_dir}/test_mc ${result_dir} 200 200 10
