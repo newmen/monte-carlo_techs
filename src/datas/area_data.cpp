a77cbedf6b7cb62cbc04fc401022a137ae06c43a
Gleb Averchuk <altermn@gmail.com>
2012-10-02 21:04:11 +0400

    NO+CO spiral concentrations distribution

--- src/datas/area_data.cpp
+++ src/datas/area_data.cpp
@@ -6,7 +6,24 @@ AreaData::AreaData(int sizeX, int sizeY) : _sizeX(sizeX), _sizeY(sizeY) {
     int i = 0;
     for (int y = 0; y < _sizeY; ++y) {
         for (int x = 0; x < _sizeX; ++x) {
-            int value = (rand() % 10 < 8) ? 1 : ((rand() % 2 == 0) ? 2 : 3);
+//            int value = (rand() % 10 < 8) ? 1 : ((rand() % 2 == 0) ? 2 : 3);
+
+            float cx1, cx2;
+            if (y < _sizeY / 2 && x < _sizeX / 2) {
+                cx1 = 0.067;    cx2 = 0.223;
+            } else if (y < _sizeY / 2 && x >= _sizeX / 2) {
+                cx1 = 0.249;    cx2 = 0.504;
+            } else if (y >= _sizeY / 2 && x >= _sizeX / 2) {
+                cx1 = 0.533;    cx2 = 0.292;
+            } else { // if (y >= _sizeY / 2 && x < _sizeX / 2)
+                cx1 = 0.319;    cx2 = 0.104;
+            }
+
+            float r = (float)(rand() % 100) / 100;
+            int value = 1; // empty value
+            if (r < cx1) value = 2;
+            else if (r < cx1 + cx2) value = 3;
+
 //            int value = (rand() % 10 < 7) ? 1 : ((rand() % 3 == 0) ? 2 : ((rand() % 2 == 0) ? 3 : 4));
 //            int value = 1;
             _cells[i++] = value;
