a8299c22fc04acce021f0d6ed23255ca46535d1b
Gleb Averchuk <altermn@gmail.com>
2013-01-22 03:26:50 +0400

    fix much optima rejection-free

--- src/contexts/muchoptimizedrejectionfreesimulation_context.cpp
+++ src/contexts/muchoptimizedrejectionfreesimulation_context.cpp
@@ -6,11 +6,11 @@ BaseEventData *MuchOptimizedRejectionFreeSimulationContext::randomEvent() const 
     long double r = randomN01() * totalRate();
     long double accRate = 0;
 
-    for (auto pRate = _cellRates.cbegin(); pRate != _cellRates.cend(); ++pRate) {
+    for (auto pRate = _dimerRates.cbegin(); pRate != _dimerRates.cend(); ++pRate) {
         accRate += pRate->second;
         if (r < accRate) {
             accRate -= pRate->second;
-            auto events = _cellEvents.find(pRate->first)->second;
+            auto &events = _dimerEvents.find(pRate->first)->second;
             for (auto pEvent = events.cbegin(); pEvent != events.cend(); ++pEvent) {
                 accRate += (*pEvent)->rate();
                 if (r < accRate) return *pEvent;
@@ -18,11 +18,11 @@ BaseEventData *MuchOptimizedRejectionFreeSimulationContext::randomEvent() const 
         }
     }
 
-    for (auto pRate = _dimerRates.cbegin(); pRate != _dimerRates.cend(); ++pRate) {
+    for (auto pRate = _cellRates.cbegin(); pRate != _cellRates.cend(); ++pRate) {
         accRate += pRate->second;
         if (r < accRate) {
             accRate -= pRate->second;
-            auto events = _dimerEvents.find(pRate->first)->second;
+            auto &events = _cellEvents.find(pRate->first)->second;
             for (auto pEvent = events.cbegin(); pEvent != events.cend(); ++pEvent) {
                 accRate += (*pEvent)->rate();
                 if (r < accRate) return *pEvent;
